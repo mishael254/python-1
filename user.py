50986849524a8c0b686c7e8bf3716e045fa80952
mishael254 <mishaelmishantoz@gmail.com>
2020-07-06 01:38:30 +0300

    create a new class and name it credentials then generate a new initialization method and pass in the params of generating a new credential account

--- user.py
+++ user.py
@@ -57,6 +57,18 @@ class User:
        return cls.user_account
 
 
+       
+   class Credentials:
+
+    def __init__ (self, first_name,last_name,number,email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+
+
 
 
 
