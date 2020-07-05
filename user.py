dc259a26073c022c7413ad3ebca0e06c03326efe
mishael254 <mishaelmishantoz@gmail.com>
2020-07-06 00:46:09 +0300

    create a new  method to save the user

--- user.py
+++ user.py
@@ -3,3 +3,6 @@ class User:
     a class that generates a new user password account after providing a username and a password
     """
     user_account = []# create an empty user_account
+
+    def save_user(self):
+        
