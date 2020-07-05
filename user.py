0c7547a04b4c95398364897b421e0e69174f6019
mishael254 <mishaelmishantoz@gmail.com>
2020-07-06 00:51:19 +0300

    a new method to remove an already created account

--- user.py
+++ user.py
@@ -5,4 +5,13 @@ class User:
     user_account = []# create an empty user_account
 
     def save_user(self):
+        """
+        new method to save the users username and password
+        """
+        User.user_account.append(self)
+
+    def delete_user(self):
+        """
+        a new method to delete users account
         
+
